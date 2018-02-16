272fd75c8022c557eafec3f49a087013e3553d68
Oleksii Iskiv <33020662+Silent777@users.noreply.github.com>
2018-02-16 16:02:09 +0200

    Fix

--- src/containers/item/ItemService.js
+++ src/containers/item/ItemService.js
@@ -1,6 +1,6 @@
 const itemService = () => {
     let items = []
-    for (var i = 0, len = localStorage.length; i < len; ++i) {
+    for (let i = 0, len = localStorage.length; i < len; ++i) {
         items.push(JSON.parse(localStorage.getItem(localStorage.key( i ))));
       }
     return items;
